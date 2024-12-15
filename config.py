074adda305f102787419811c7e040678ee2f0f2f
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2024-12-15 11:44:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
+API_ID = int(getenv("API_ID", "13828860"))
+API_HASH = getenv("API_HASH", "bbcd5f94dcd9f8a5eedc5de7397b4127")
+BOT_TOKEN = getenv("BOT_TOKEN", "7964164690:AAEM3L-dmniN5uj6LvosfIhjOdIXCbgJpT4")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1001611423558"))
